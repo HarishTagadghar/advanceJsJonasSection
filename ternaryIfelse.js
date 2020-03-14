29f3642b9f790181669180e50db546669eb7ec7a
harish tagadghar <harishtgtg@gmail.com>
2020-03-14 11:32:17 -0700

    ternaryIfelse.js

---
+++ ternaryIfelse.js
@@ -0,0 +1,7 @@
+let age = 14;
+age >=18 ? console.log('he can drink beer') : console.log('he drinks juice');
+
+// this tertiaryIfelse method is usually used to diclare a varible on condition
+
+let drink = age >= 18 ?  'beer' : 'juice';
+console.log(drink);
