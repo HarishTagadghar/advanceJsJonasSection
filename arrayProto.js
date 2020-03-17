a640956599b82500ae89a7d273a0f3ecda29234f
harish tagadghar <harishtgtg@gmail.com>
2020-03-17 14:12:49 +0530

    explorign how the method of array are used

---
+++ arrayProto.js
@@ -0,0 +1,6 @@
+// as we know array is also a object array is alo stored in a object
+let arrays = [1,2,3];
+// info shows all the information if thet variable
+// all the method of array { pop,slice,shift etc} are stored in the object proto and also the length property is stored in this object
+// this object of arrays variable is not shown in the terminal it is visable in the console of chrome brouser
+console.info(arrays)
